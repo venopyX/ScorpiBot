6818e33f966f28fdceef848b0aee97dd477b54f3
Gemechis Chala <gladsonchala@gmail.com>
2025-06-23 06:34:58 +0300

    chore: remove unnecessary files

--- config.py
+++ config.py
@@ -8,6 +8,4 @@ load_dotenv()
 API_BASE_URL = os.getenv("API_BASE_URL")
 API_TOKEN = os.getenv("API_TOKEN")
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
